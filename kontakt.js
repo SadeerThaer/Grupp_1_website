40aed434e3022d47d47892496d613178d07ad136
Sadeer Thaer <45896436+SadeerThaer@users.noreply.github.com>
2019-09-23 16:51:54 +0200

    SlideShow

--- kontakt.js
+++ kontakt.js
@@ -1,26 +1,63 @@
-function getAlla(){
-    var klaga = document.getElementById('meddelande').value;    
-    var fnamn = document.getElementById('fnamn').value;  
-    var enamn = document.getElementById('enamn').value;  
-    var telefon = parseInt(document.getElementById('telefon').value, 10);  
-    var email = document.getElementById('email').value;
+
+function boxValid(valdBox){
     
-    var hela = klaga+fnamn+enamn+telefon+email;
-    document.getElementById('testatext').innerHTML= hela;
-    inteTom(email);
+    var box = document.getElementById(valdBox).value;  
+    var valid = false;
+   
+    switch (valdBox) {
+        case 'fnamn':
+        valid = /^([a-zA-Zåäö]{3,})*$/.test(box);
+           return valid;
+        case 'enamn':
+            valid = /^([a-zA-Zåäö]{3,})*$/.test(box);
+            return valid;
+        case 'telefon':
+            valid = /^([0-9]{3,})*$/.test(box);
+            return valid;
+        case 'email':
+            valid = /[^@]+@[^@]+/.test(box);
+            return valid;
+        case 'meddelande':
+            valid = /^([a-zA-Zåäö0-9.!?,]{3,})*$/.test(box);
+            return valid;
+      }
 }
 
-function inteTom(email){
-    var valid = /[a]/.test(email);
-    //var valid = /^\s*$/.test(email);
-    //var valid = /[^@]+@[^@]+/.test(email);
+
+
+function boxColor(valdBox){
+    var valid = false;
+   valid =  boxValid(valdBox);
     if(valid){
-    alert("Mail is right");
-}
+        boxColorGreen(valdBox);
+
+    }
     else{
-        alert("wrong");
+        boxColorRed(valdBox);
     }
 
 }
-    
 
+function boxColorRed(valdBox){
+
+    $('#' + valdBox).css("border-color", "crimson");
+}
+
+function boxColorGreen(valdBox){
+
+    $('#' + valdBox).css("border-color", "green");
+}
+
+function clearBoxar(){
+    document.getElementById("fnamn").value=""; 
+    document.getElementById("enamn").value=""; 
+    document.getElementById("telefon").value=""; 
+    document.getElementById("email").value=""; 
+    document.getElementById("meddelande").value=""; 
+
+    $('#fnamn').css("border-color", "");
+    $('#enamn').css("border-color", "");
+    $('#telefon').css("border-color", "");
+    $('#email').css("border-color", "");
+    $('#meddelande').css("border-color", "");
+}
